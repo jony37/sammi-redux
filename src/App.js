94caa0c33bd80b7d8b87a66f9dab2ad6ae128e7f
jony <ravshanovjony7@gmail.com>
2025-08-13 23:59:04 +0500

    4-dars

---
+++ src/App.js
@@ -0,0 +1,11 @@
+
+import Counter from "./components/Counter";
+
+const App = () => {
+  return <div className="container">
+    <h1>Counter: 0</h1>
+    <Counter/>
+  </div>;
+};
+
+export default App;
---
+++ src/components/Counter.jsx
@@ -0,0 +1,11 @@
+
+const Counter = () => {
+  return (
+    <div className="btn-group">
+        <button></button>
+        <button></button>
+    </div>
+  )
+}
+
+export default Counter
